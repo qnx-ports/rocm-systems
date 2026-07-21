709545e64a4c23bc2b34c637755d4e962ff92f60
ggottipa-amd <ggottipa@amd.com>
2026-07-21 08:38:47 +0000

    adding checks for rocprofv3

--- projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_base.py
+++ projects/rocprofiler-compute/src/rocprof_compute_profile/profiler_base.py
@@ -363,6 +363,7 @@ class RocProfCompute_Base:
                 workload_dir=args.output_directory,
                 ml_api_trace_enabled=bool(getattr(self, "_selected_frameworks", set())),
                 retain_rocpd_output=args.retain_rocpd_output,
+                tool_path=getattr(args, "rocprofiler_sdk_tool_path", None),
             )
 
             end_time = time.time()
