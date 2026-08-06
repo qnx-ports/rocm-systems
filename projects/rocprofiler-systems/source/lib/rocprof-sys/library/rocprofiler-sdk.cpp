24d37eb0b1f26b497bdc434d84ca1c1202bd6aea
Milan Radosavljevic <milan.radosavljevic@amd.com>
2026-08-06 11:12:31 +0200

    remove kfd gate at 1.0.0 and move it to 1.2.2

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk.cpp
@@ -2097,7 +2097,7 @@ tool_tracing_buffered(rocprofiler_context_id_t /*context*/,
                 }
             }
 #endif
-#if (ROCPROFILER_VERSION >= 10000)
+#if (ROCPROFILER_VERSION >= 10202)
             else if(header->kind == ROCPROFILER_BUFFER_TRACING_KFD_PAGE_FAULT)
             {
                 auto* record =
@@ -2641,7 +2641,7 @@ tool_init(rocprofiler_client_finalize_t fini_func, void* user_data)
     }
 #endif
 
-#if (ROCPROFILER_VERSION >= 10000)
+#if (ROCPROFILER_VERSION >= 10202)
     // Initialize KFD event metadata
     if(_buffered_domain.count(ROCPROFILER_BUFFER_TRACING_KFD_PAGE_FAULT) > 0 ||
        _buffered_domain.count(ROCPROFILER_BUFFER_TRACING_KFD_PAGE_MIGRATE) > 0 ||
